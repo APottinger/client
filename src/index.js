509db665e41ad1913eafa4e26c4be8c596d82904
Allen Pottinger <allenpottinger@gmail.com>
2021-12-13 19:44:24 -0500

    adds redux and thunk

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import reducers from './api/reducers'
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
 ReactDOM.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
