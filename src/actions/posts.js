e8f0e405e38e175f34b03493c720fcc714c60d53
Allen Pottinger <allenpottinger@gmail.com>
2021-12-15 14:07:20 -0500

    creates posts action creator

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,1 +1,13 @@
-import * as api from '../index'
+import * as api from '../api'
+
+const getPosts = () => async (dispatch) => {
+    try {
+        const { data } = await api.fetchPosts();
+        dispatch( { type: 'FETCH_POSTS', payload: data })
+    } catch (err) {
+        console.log(err.message);
+    }
+
+}
+
+export default getPosts;
