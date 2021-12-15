b5ad8f80e9c4e4b18cf0e90ba9438a9c4be8ea65
Allen Pottinger <allenpottinger@gmail.com>
2021-12-15 13:07:37 -0500

    updates file tree

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
+import getPosts from './api/'
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
 const App = () => {
+
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    dispatch(getPosts());
+  }, []);
+
   return (
     <div>
       <h1>Nostalgia</h1>
