8dc202e3682d747c15c4595bd90a4bc114735379
Allen Pottinger <allenpottinger@gmail.com>
2021-12-20 19:03:49 -0500

    creates form using rhf

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Form = () => {
+    const {register, handleSubmit, watch, formState: { errors }} = useForm();
+    
+
+    const onSubmit = (data) => {
+        console.log(data)
+    }
+
     return (
+        <>
         <h3>Form</h3>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input type="textarea" placeholder="your post..." {...register('post', { required: true })}/>
+            <input type="text" placeholder="name" {...register('name', { required: true})}/>
+            {errors.post && errors.post.errors}
+            <input type="submit" />
+        </form>
+
+        </>
     )
 }
 
